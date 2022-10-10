3d967e47a18e717e15b5c004cfdd8f59c5eafdcf
Alexandr <aleksandr.izis@mail.ru>
2022-10-10 09:54:24 +0400

    add bottom navigation

--- app/src/main/java/com/example/module1/MainActivity.kt
+++ app/src/main/java/com/example/module1/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.module1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +11,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val navigation: BottomNavigationView = findViewById(R.id.btnNav)
+        navigation.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.news -> {
+                    Toast.makeText(this, "news", Toast.LENGTH_LONG)
+                    true
+                }
+                else -> {
+                    Toast.makeText(this, "else", Toast.LENGTH_LONG)
+                    true
+                }
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
