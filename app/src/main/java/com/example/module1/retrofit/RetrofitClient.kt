fb62ee1688dd7d7f13e4aff263d45682c1b2a252
Alexandr <aleksandr.izis@mail.ru>
2022-12-08 14:28:30 +0400

    add logging

--- app/src/main/java/com/example/module1/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/example/module1/retrofit/RetrofitClient.kt
@@ -1,14 +1,21 @@
 package com.example.module1.retrofit
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
     fun getClient(baseUrl: String): Retrofit {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BASIC)
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(logging)
         return Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(httpClient.build())
             .build()
     }
 }
