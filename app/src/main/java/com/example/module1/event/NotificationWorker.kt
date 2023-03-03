468efd3a2b44f0c5cda192f3b3b7e8933a40fc54
Alexandr <aleksandr.izis@mail.ru>
2023-03-03 11:48:32 +0400

    add button to notification

--- app/src/main/java/com/example/module1/event/NotificationWorker.kt
+++ app/src/main/java/com/example/module1/event/NotificationWorker.kt
@@ -2,12 +2,15 @@ package com.example.module1.event
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.example.module1.R
+import com.example.module1.news.NewsComposeActivity
 
 class NotificationWorker(
     val context: Context,
@@ -15,10 +18,14 @@ class NotificationWorker(
 ) : Worker(context, workerParams) {
     override fun doWork(): Result {
         createChannel()
+        val intent = Intent(context, NewsComposeActivity::class.java)
+        val pendingIntent =
+            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT)
         val builder = NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.avatar_1)
             .setContentTitle(inputData.getString("name"))
             .setContentText("Спасибо!!! ")
+            .addAction(R.drawable.ic_access_time, "Напомнить позже", pendingIntent)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setStyle(
                 NotificationCompat.BigTextStyle().bigText(
@@ -26,6 +33,7 @@ class NotificationWorker(
                         "Будем очень признательны, если вы сможете пожертвовать еще больше."
                 )
             )
+            .setAutoCancel(true)
             .build()
         val notificationManager = NotificationManagerCompat.from(context)
         notificationManager.notify(NOTIFICATION_ID, builder)
