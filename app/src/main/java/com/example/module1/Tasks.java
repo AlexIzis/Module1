abe949571c7bcaad7b362d0afa9ef0063a26f191
Alexandr <aleksandr.izis@mail.ru>
2023-03-13 22:20:00 +0400

    full

--- app/src/main/java/com/example/module1/Tasks.java
+++ app/src/main/java/com/example/module1/Tasks.java
@@ -74,4 +74,64 @@ public class Tasks {
     public static boolean validateUsr(String s) {
         return s.matches("[a-z0-9_]{4,16}");
     }
+
+    // +2 8 kyu
+    /*
+    https://www.codewars.com/kata/551b4501ac0447318f0009cd
+     */
+    public static String convert(boolean b) {
+        return String.valueOf(b);
+    }
+
+    /*
+    https://www.codewars.com/kata/57a2013acf1fa5bfc4000921
+     */
+    public static double find_average(int[] array) {
+        int result = 0;
+        if (array != null) {
+            for (int i = 0; i < array.length; i++) {
+                result += array[i];
+            }
+
+            return result / array.length;
+        }
+
+        return 0;
+    }
+
+    // +2 7 kyu
+    /*
+    https://www.codewars.com/kata/5296455e4fe0cdf2e000059f
+     */
+    public static Double calculate(final double numberOne, final String operation, final double numberTwo)
+    {
+        switch (operation) {
+            case "+" :
+                return numberOne + numberTwo;
+            case "-" :
+                return numberOne - numberTwo;
+            case "*" :
+                return numberOne * numberTwo + 0.0;
+            case "/" :
+                return numberTwo != 0 ? numberOne / numberTwo : null;
+        }
+        return null;
+    }
+
+    /*
+    https://www.codewars.com/kata/57aa218e72292d98d500240f
+     */
+    public static double heron(double a, double b, double c)
+    {
+        double s = (a + b + c) / 2;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+
+    // +1 8 kyu
+    /*
+    https://www.codewars.com/kata/5208f99aee097e6552000148
+     */
+    public static String camelCase(String input) {
+        return input.replaceAll("([A-Z])", " $1");
+    }
 }
